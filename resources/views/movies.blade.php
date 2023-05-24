25af95e72aa5a18b0c439a2a74da340b00f86342
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-05-24 15:45:29 +0200

    print card and movies info

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,11 +2,34 @@
 
 @section('content')
     <h2>MOVIE</h2>
-    <ul>
-        @forelse ($movies as $movie)
-            <li>{{ $movie->title }}</li>
-        @empty
-            <h1>Niente</h1>
-        @endforelse
-    </ul>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 pt-4">
+            @forelse ($movies as $movie)
+                <div class="col">
+                    <div class="card shadow h-100">
+                        <div class="card-body d-flex flex-column">
+                            <h2 class="card-title">{{ $movie->title }}</h2>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <h6>{{ $movie->original_title }}</h6>
+                                </li>
+                                <li>
+                                    <h6>{{ $movie->original_nationality }}</h6>
+                                </li>
+                                <li>
+                                    <h6>{{ $movie->date }}</h6>
+                                </li>
+                                <li>
+                                    <h6>{{ $movie->vote }}</h6>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /card body -->
+                    </div>
+                    <!-- /card -->
+                </div>
+            @empty
+            @endforelse
+        </div>
+    </div>
 @endsection
